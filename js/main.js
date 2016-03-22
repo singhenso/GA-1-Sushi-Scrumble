be248fe65096022dd99545666b2279ec7ac7aa4e
Gagandeep Singh <singh.enso@gmail.com>
2016-03-21 18:57:15 -0700

    Added table

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-// function initElement() {
-//   var p = document.getElementById("foo");
-//   // NOTE: showAlert(); or showAlert(param); will NOT work here.
-//   // Must be a reference to a function name, not a function call.
-//   p.onclick = showAlert;
-// };
-
-// function showAlert() {
-//   alert("onclick Event detected!")
-// }
 
 
 $('#btn1').on('click', function() {
@@ -29,3 +19,6 @@ $('.ingredient').on('click', function() {
   // we are trying to console what we click on
   //we have to put an event listener, and target the things we want to click on
   // we are trying to type out what the text of that particular dom node.
+
+
+document.getElementByID(element ID)
