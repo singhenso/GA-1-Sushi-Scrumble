4e4a9f8ee67fbbde35d28873e9c681af6ae17750
Gagandeep Singh <singh.enso@gmail.com>
2016-03-22 10:39:29 -0700

    new commit

--- js/main.js
+++ js/main.js
@@ -1,54 +1,82 @@
+// shrimp sushi = rice, seweed , shrimp
 
+var winningcombo = ['Shrimp', 'Seaweed', 'Rice'];
+
+// when the user clicks on a button, get the text  and add it
+// to the playersChoices array
+var playersChoices = [];
 
 $('#btn1').on('click', function() {
-  var gamebegin =$(this).text() ;
+  var gamebegin = $(this).text();
   console.log(gamebegin);
 });
 
 $('#btn2').on('click', function() {
   var instructions =$(this).text();
-  console.log(instructions);
+  playersChoices.push(instructions);
 });
 
 $('#cell1').on('click', function() {
   var cell1 = $(this).text();
-  console.log(cell1);
+  playersChoices.push(cell1.trim());
 });
 
 $('#cell2').on('click', function() {
   var cell2 = $(this).text();
-  console.log(cell2);
+  playersChoices.push(cell2.trim());
 });
 $('#cell3').on('click', function() {
   var cell3 = $(this).text();
-  console.log(cell3);
+  playersChoices.push(cell3.trim());
 });
 $('#cell4').on('click', function() {
   var cell4 = $(this).text();
-  console.log(cell4);
+  playersChoices.push(cell4.trim());
 });
 $('#cell5').on('click', function() {
   var cell5 = $(this).text();
-  console.log(cell5);
+  playersChoices.push(cell5.trim());
 });
 $('#cell6').on('click', function() {
   var cell6 = $(this).text();
-  console.log(cell6);
+  playersChoices.push(cell6.trim());
 });
 $('#cell7').on('click', function() {
   var cell7 = $(this).text();
-  console.log(cell7);
+  playersChoices.push(cell7.trim())
 });
 $('#cell8').on('click', function() {
   var cell8 = $(this).text();
-  console.log(cell8);
+  playersChoices.push(cell8.trim())
 });
 $('#cell9').on('click', function() {
   var cell9 = $(this).text();
-  console.log(cell9);
+  playersChoices.push(cell9.trim());
 });
 
+var checkwin = function() {
+  // loop through the players choices
+  // if the choice is in the winning array it's good
+  // if it's not, they lose
+  for (var i=0; i< playersChoices.length; i++) {
+    var choice = playersChoices[i];
+    // if the choice is not there
+    if (winningcombo.indexOf(choice) === -1) {
+      console.log('wrong choice');
+      // return early
+      return false;
+    }
+  };
+  // if we don't return from the function early
+  // return true
+  console.log('right choice');
+  return true;
+};
 
+// when teh player has selected three ingredients
+// check and see if they're in the winningcombo
+// if they're all there they win
+// if they're not, they lose
 
 
   // we are trying to console what we click on
