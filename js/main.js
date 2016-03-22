81a1a0301da4bb6f9cb1e5a30043fb9a968f99c2
Gagandeep Singh <singh.enso@gmail.com>
2016-03-21 17:11:11 -0700

    add commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log('this works!')
+// function initElement() {
+//   var p = document.getElementById("foo");
+//   // NOTE: showAlert(); or showAlert(param); will NOT work here.
+//   // Must be a reference to a function name, not a function call.
+//   p.onclick = showAlert;
+// };
+
+// function showAlert() {
+//   alert("onclick Event detected!")
+// }
+
+
+$('#btn1').on('click', function() {
+  alert('Let the Games Begin!');
+});
+
+$('#btn2').on('click', function() {
+  alert('These are the Instructions');
+});
+
+$('.ingredient').on('click', function() {
+  var opt2 = $(this).text();
+  console.log(opt2);
+});
+
+
+
+
+  // we are trying to console what we click on
+  //we have to put an event listener, and target the things we want to click on
+  // we are trying to type out what the text of that particular dom node.
