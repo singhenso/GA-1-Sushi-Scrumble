a673c8bb939504ee986e7080214ec21ae64803d4
Gagandeep Singh <singh.enso@gmail.com>
2016-06-06 17:10:10 -0700

    new commit

---
+++ js/main.js
@@ -0,0 +1,165 @@
+//This is the splash screen that sets a timeout for make a sushi prompt.
+$('.enter_link').click(function () {
+    $(this).parent('#splashscreen').fadeOut(500);
+    setTimeout(function() { getSushi(); }, 1000);
+});
+//These are the 8 possible winning combos to score 100 points
+var winningcombo =[['Shrimp', 'Seaweed', 'Rice'],
+                   ['Tuna', 'Seaweed', 'Rice'],
+                   ['Scallop', 'Seaweed', 'Rice'],
+                   ['Salmon', 'Seaweed', 'Rice'],
+                   ['Squid', 'Seaweed', 'Rice'],
+                   ['Snapper', 'Seaweed', 'Rice'],
+                   ['Lobster', 'Seaweed', 'Rice'],
+                   ['Scallop', 'Seaweed', 'Rice']];
+//Empty array which gets filled with whatever the player clicks on
+var playersChoices = [];
+//targetSushi is assigned the random winning combo with 8 arrays.
+var targetSushi;
+//Tells the player to make 1 of the 8 possible sushies
+var getSushi = function() {
+  targetSushi = winningcombo[Math.floor(Math.random()*7)];
+  alert('Make me a ' + targetSushi[0] + ' Sushi!');
+};
+//These are the 9 ingredients. If clicked, addes that ingredient
+//into the PLayerChoices array
+//Trim removes any empty space
+//Once the player clicks three ingredients, it runs the Checkwin function.
+$('#cell1').on('click', function() {
+  $('#newChoice').append('<img src="./images/rice.jpg" width = 100px />');
+  playersChoices.push('Rice');
+  if (playersChoices.length === 3) {
+    checkWin();
+    playersChoices = [];
+  }
+});
+
+$('#cell2').on('click', function() {
+  $('#newChoice').append('<img src="./images/sea_weed.png" width = 100px />');
+  playersChoices.push('Seaweed');
+  if (playersChoices.length === 3) {
+    checkWin();
+    playersChoices = [];
+  }
+});
+$('#cell3').on('click', function() {
+  $('#newChoice').append('<img src="./images/tuna.jpg" width = 100px />');
+  playersChoices.push('Tuna');
+   if (playersChoices.length === 3) {
+    checkWin();
+    playersChoices = [];
+  }
+});
+$('#cell4').on('click', function() {
+  $('#newChoice').append('<img src="./images/shrimp.jpg" width = 100px />');
+  playersChoices.push('Shrimp');
+   if (playersChoices.length === 3) {
+    checkWin();
+    playersChoices = [];
+  }
+});
+$('#cell5').on('click', function() {
+  $('#newChoice').append('<img src="./images/salmon.jpg" width = 100px />');
+  playersChoices.push('Salmon');
+   if (playersChoices.length === 3) {
+    checkWin();
+    playersChoices = [];
+  }
+});
+$('#cell6').on('click', function() {
+  $('#newChoice').append('<img src="./images/squid.jpg" width = 100px />');
+  playersChoices.push('Squid');
+   if (playersChoices.length === 3) {
+    checkWin();
+    playersChoices = [];
+  }
+});
+$('#cell7').on('click', function() {
+  $('#newChoice').append('<img src="./images/snapper.jpg" width = 100px />');
+  playersChoices.push('Snapper');
+   if (playersChoices.length === 3) {
+    checkWin();
+    playersChoices = [];
+  }
+});
+$('#cell8').on('click', function() {
+  $('#newChoice').append('<img src="./images/lobster.jpg" width = 100px />');
+  playersChoices.push('Lobster');
+   if (playersChoices.length === 3) {
+    checkWin();
+    playersChoices = [];
+  }
+});
+$('#cell9').on('click', function() {
+  $('#newChoice').append('<img src="./images/scallop.jpg" width = 100px />');
+  playersChoices.push('Scallop');
+   if (playersChoices.length === 3) {
+    checkWin();
+    playersChoices = [];
+  }
+});
+
+//Score is a variable that holds all the score, starting from 0.
+var score = 0;
+
+//this is a function that puts the current score in the scoreboard div.
+function showScore() {
+  $('#scoreboard').text(score);
+}
+
+//If player scores over 500, the Game is Over is the player Wins!
+function gameOver() {
+  if (score === 500) {
+    alert('You won the Game! Congrats!');
+    score = 0;
+ } else if (score <= 0) {
+  alert('You made the Wrong Sushi!');
+  if (score === -300) {
+    alert('You have Lost the Game! Try again');
+  }
+ }
+}
+
+var randomizer = function() {
+    var randomTable =  $('#column').get().sort(function() {
+            return Math.round(Math.random())-0.5;
+           });
+           $(randomTable).appendTo(randomTable[0].parentNode).show();
+         };
+
+//the showScore function is delayed for 1 second
+setTimeout(function() { showScore; }, 1000);
+//checkWin functions runs everytime player clicks on ingredients to
+//compare it to the winning choices.
+//if player clicks wrong move, they get deduceted 100 points. If win, they get 100 oints.
+var checkWin = function() {
+var wrongAnswer = true;
+    var count = 0;
+    var recipe = targetSushi;
+    for (var j=0; j<playersChoices.length; j++) {
+      var choice=playersChoices[j];
+      if (recipe.indexOf(choice) === j) {
+        console.log('you got one right ');
+        count++;
+        if (count === 3) {
+          alert('You made the Sushi with correct ingredients!');
+          wrongAnswer = false;
+          score= score + 100;
+          showScore();
+          gameOver();
+          randomizer();
+         $("#newChoice img").remove();
+        }
+        continue;
+      }
+  }
+  if(wrongAnswer){
+    score= score - 100;
+    showScore();
+    gameOver();
+    randomizer();
+    $("#newChoice img").remove();
+
+  }
+//getSushi is a function that pops up within 2 seconds of starting game
+getSushi();};
